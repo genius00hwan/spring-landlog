105e737bee87904f5266116fada30b75163e5f2e
genius00hwan <qnfn120@gmail.com>
2023-07-19 00:17:00 +0900

    [1. 로그인 구현] 김수환 과제 제출합니다. (#16)
    fix (MemberController) : add try catch logic when create member
    
    fix path
    
    fix(README) : add how implement
    
    fix(README) :
    
    fix(MemberServiceTest) : add func to validate join test
    
    fix(MemberService) : add func to validate join
    
    fix(MemberController) : fix path to lgin
    
    fix(MemoryMemberRepositoryTest) : add functions
    
    fix(MemberServiceTest) : add functions
    
    fix(MemberService) : fix validateDuplicateMember
    
    create(blogList) : create blogList.html
    
    feat(LoginForm) : crate logInForm.html
    
    feat(LoginController) : creat LoginController
    
    fix(MemberService) : fix findOne method to findByEmail
    
    feat(LandLogController) : create LandLogController
    
    feat(BlogController) : create BlogController interface
    
    fix(templates) : fix html

--- src/main/java/com/landvibe/landlog/controller/MemberForm.java
+++ src/main/java/com/landvibe/landlog/controller/MemberForm.java
@@ -1,13 +1,25 @@
 package com.landvibe.landlog.controller;
 
 public class MemberForm {
-    private String name;
+	private String name;
+	private String password;
+	private String email;
 
-    public String getName() {
-        return name;
-    }
+	public MemberForm(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
 }
