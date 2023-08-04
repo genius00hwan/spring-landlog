50e4c1b53f776b4efc322edd9a25f79574aa321a
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-08-04 21:32:18 +0900

    fix : Add validate creator and blog logic

--- src/main/java/com/landvibe/landlog/controller/BlogApiController.java
+++ src/main/java/com/landvibe/landlog/controller/BlogApiController.java
@@ -4,46 +4,47 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
 
-import com.landvibe.landlog.controller.form.BlogForm;
 import com.landvibe.landlog.domain.Blog;
 import com.landvibe.landlog.service.BlogService;
-import com.landvibe.landlog.service.MemberService;
 
 @RestController
 @RequestMapping("v1/api/blogs")
 public class BlogApiController {
 
 	private final BlogService blogService;
-	private final MemberService memberService;
-
-	public BlogApiController(BlogService blogService, MemberService memberService) {
+	public BlogApiController(BlogService blogService) {
 		this.blogService = blogService;
-		this.memberService = memberService;
 	}
 
 	@GetMapping
 	public List<Blog> list(@RequestParam Long creatorId) {
+		blogService.validateCreator(creatorId);
 		return blogService.findBlogList(creatorId);
 	}
 
 	@PostMapping
 	public Blog create(@RequestParam Long creatorId, @RequestBody Blog blog) {
-		memberService.findById(creatorId);
+		blogService.validateCreator(creatorId);
+		blogService.validateBlog(blog.getTitle(), blog.getContents());
 		return blogService.create(blog);
 	}
 
 	@GetMapping(value = "/{blogId}")
 	public Blog get(@RequestParam Long creatorId, @PathVariable("blogId") Long blogId) {
+		blogService.validateCreator(creatorId);
 		return blogService.findByBlogId(blogId);
 	}
 
 	@PutMapping(value = "/{blogId}")
 	public Blog update(@RequestParam Long creatorId, @PathVariable("blogId") Long blogId, @RequestBody Blog blog) {
+		blogService.validateCreator(creatorId);
+		blogService.validateBlog(blog.getTitle(), blog.getContents());
 		return blogService.update(blog);
 	}
 
 	@DeleteMapping(value = "/{blogId}")
 	public void delete(@RequestParam Long creatorId, @PathVariable("blogId") Long blogId) {
+		blogService.validateCreator(creatorId);
 		blogService.delete(blogId);
 	}
 }
