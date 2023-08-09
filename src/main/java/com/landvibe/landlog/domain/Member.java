6eb9b45b1316eabe11f07cd22f9e1910b5a5e5ac
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-08-09 17:40:13 +0900

    fix(Member) : apply lombok pattern

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -1,5 +1,12 @@
 package com.landvibe.landlog.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class Member {
 
 	private Long id;
@@ -7,29 +14,19 @@ public class Member {
 	private String email;
 	private String password;
 
+	public Member(){}
+
 	public Member(String name, String email, String password) {
 		this.name = name;
 		this.email = email;
 		this.password = password;
 	}
 
-	public void setId(Long id) {
+	public Member(Long id, String name, String email, String password) {
 		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
 }
