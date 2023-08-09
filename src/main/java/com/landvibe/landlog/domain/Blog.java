84c74333225c90aac4ec353732a7b0c27b67795e
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-08-09 17:39:50 +0900

    fix(Blog) : apply lombok pattern

--- src/main/java/com/landvibe/landlog/domain/Blog.java
+++ src/main/java/com/landvibe/landlog/domain/Blog.java
@@ -1,5 +1,12 @@
 package com.landvibe.landlog.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class Blog {
 
 	private Long id;
@@ -7,41 +14,16 @@ public class Blog {
 	private String title;
 	private String contents;
 
+	public Blog(){}
 	public Blog(Long creatorId, String title, String contents) {
 		this.creatorId = creatorId;
 		this.title = title;
 		this.contents = contents;
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
+	public Blog(Long creatorId, Long id, String title, String contents) {
 		this.id = id;
-	}
-
-	public Long getCreatorId() {
-		return creatorId;
-	}
-
-	public void setCreatorId(Long creatorId) {
 		this.creatorId = creatorId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
 		this.title = title;
-	}
-
-	public String getContents() {
-		return contents;
-	}
-
-	public void setContents(String contents) {
 		this.contents = contents;
 	}
 
