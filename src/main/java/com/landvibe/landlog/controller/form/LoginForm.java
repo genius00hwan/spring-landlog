8dca4264c169bf5bdc5a91508eef80023e9461e5
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-08-09 17:40:35 +0900

    fix(loginForm) : apply lombok pattern

--- src/main/java/com/landvibe/landlog/controller/form/LoginForm.java
+++ src/main/java/com/landvibe/landlog/controller/form/LoginForm.java
@@ -1,5 +1,12 @@
 package com.landvibe.landlog.controller.form;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class LoginForm {
 	private final String email;
 	private final String password;
@@ -7,13 +14,5 @@ public class LoginForm {
 	public LoginForm(String email, String password) {
 		this.email = email;
 		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPassword() {
-		return password;
 	}
 }
