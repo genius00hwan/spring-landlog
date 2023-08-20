03dc81b85682aa96cb365d44a065fdf2d6ecd4c4
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-08-20 17:03:24 +0900

    fix : apply MemberApiController

--- src/main/java/com/landvibe/landlog/controller/MemberController.java
+++ src/main/java/com/landvibe/landlog/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.landvibe.landlog.controller;
 
 import com.landvibe.landlog.controller.form.LoginForm;
-import com.landvibe.landlog.controller.form.MemberForm;
 import com.landvibe.landlog.domain.Member;
 import com.landvibe.landlog.service.MemberService;
 
@@ -15,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 
 @Controller
+@RequestMapping("/members")
 public class MemberController {
 	private final MemberService memberService;
 
@@ -22,40 +22,31 @@ public class MemberController {
 		this.memberService = memberService;
 	}
 
-	@GetMapping(value = "/members/new")
+	@GetMapping(value = "/new")
 	public String createForm() {
 		return "members/createMemberForm";
 	}
 
-	@PostMapping(value = "/members/new")
-	public String create(MemberForm form) {
-
-		Member member = Member.builder()
-			.name(form.getName())
-			.email(form.getEmail())
-			.password(form.getPassword())
-			.build();
-
-		memberService.join(member);
+	@PostMapping(value = "/new")
+	public String create() {
 		return "redirect:/";
 	}
 
-	@GetMapping(value = "/members")
+	@GetMapping
 	public String list(Model model) {
 		List<Member> members = memberService.findMembers();
 		model.addAttribute("members", members);
 		return "members/memberList";
 	}
 
-	@GetMapping(value = "/members/login")
+	@GetMapping(value = "/login")
 	public String createLogInForm() {
 		return "/members/loginForm";
 	}
 
-	@PostMapping(value = "/members/login")
+	@PostMapping(value = "/login")
 	public String logIn(LoginForm logInForm, RedirectAttributes redirectAttributes) {
-		Long memberId;
-		memberId = memberService.logIn(logInForm.getEmail(), logInForm.getPassword());
+		Long memberId = memberService.logIn(logInForm.getEmail(), logInForm.getPassword());
 		redirectAttributes.addAttribute("creatorId", memberId);
 		return "redirect:/blogs";
 	}
