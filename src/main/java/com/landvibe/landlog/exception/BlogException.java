d0d79f3f6be4bddac869ad36565ca41f252d2a5f
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-08-22 01:10:14 +0900

    add : add toString method that make string include httpStatus and errorMessage override getMessage that return errorMessage

--- src/main/java/com/landvibe/landlog/exception/BlogException.java
+++ src/main/java/com/landvibe/landlog/exception/BlogException.java
@@ -1,10 +1,17 @@
 package com.landvibe.landlog.exception;
 
 public class BlogException extends RuntimeException {
-	ErrorMessages errorMessage;
+	public ErrorMessages errorMessage;
 
 	public BlogException(ErrorMessages errorMessage) {
 		this.errorMessage = errorMessage;
 	}
 
+	public String toString() {
+		return errorMessage.getHttpStatus() + "(" + errorMessage.getErrorMessage() + ")";
+	}
+	@Override
+	public String getMessage(){
+		return errorMessage.getErrorMessage();
+	}
 }
